f23cd81a1d2593ab40e4881638a975ceb800f0b8
xofbd <dfox09@gmail.com>
2018-04-03 21:17:19 -0400

    minor edits

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 # The purpose of this script is, given a stock ticker symbol, call the stock
 # ticker function and create a Bokeh plot running on Flask.
 
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, render_template, request
 from stock_ticker import create_plot
 
 app = Flask(__name__)
@@ -25,10 +25,10 @@ def index():
 @app.route('/plot', methods=['POST'])
 def plot():
     app.vars['ticker'] = request.form['ticker'].upper()
+
     script, div = create_plot(app.vars['ticker'])
     return render_template('plot.html', symbol=app.vars['ticker'],
                            script=script, div=div)
 
 if __name__ == '__main__':
-    # app.run(port=33507)
-	app.run(host='0.0.0.0')
+    app.run(port=33507, debug=False)
